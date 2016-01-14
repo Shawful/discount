8216ef44c3f0d2231a2ab8725b2346d7a34bc5c6
Shawn Shaw <shawn.e.shaw@gmail.com>
2016-01-14 11:38:09 -0600

    state now maintained in myDiscount service

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,38 +10,15 @@
 angular.module('discountApp')
   .controller('MainCtrl', [ 'myDiscount', '$scope', '$timeout', function (myDiscount, $scope, $timeout) {
    
-  	// Initialize variables
-    $scope.discount = .75;
-    $scope.prices = {
-    	standard: {
-    		base: 6,
-    		discount: 3,
-    		endpoint: "REGULAR_STD"
-    	},
-    	pro: {
-    		base: 10,
-    		discount: 5,
-    		endpoint: "REGULAR_PRO"
-    	},
-    	business: {
-    		base: 14,
-    		discount: 7,
-    		endpoint: "REGULAR_BUS"
-    	}
-    }
-
     // Get variables from service
+    $scope.prices = myDiscount.getPrices();
     $scope.discount = myDiscount.getDiscount();
-    
+
     // Set new discount after 5 seconds
     $timeout(function () {
     	myDiscount.setDiscount(1);
-    	$scope.discount = myDiscount.getDiscount()
-    }, 5000);
-
-    function getSrv(name, element) {
-    	element = element || '*[ng-app]';
-    	return angular.element(element).injector().get(name);
-    }
+    	$scope.discount = myDiscount.getDiscount();
+    	$scope.prices = myDiscount.getPrices();
+    }, 2000);
 
   }]);
--- app/scripts/services/myDiscount.js
+++ app/scripts/services/myDiscount.js
@@ -1,14 +1,37 @@
 angular.module('discountApp')
   .factory('myDiscount', function () {
   	var discount = .5;
+  	var prices = {
+    	standard: {
+    		base: 6,
+    		discount: 3,
+    		endpoint: "REGULAR_STD"
+    	},
+    	pro: {
+    		base: 10,
+    		discount: 5,
+    		endpoint: "REGULAR_PRO"
+    	},
+    	business: {
+    		base: 14,
+    		discount: 7,
+    		endpoint: "REGULAR_BUS"
+    	}
+    };
 
   	return {
-  		getDiscount: function () {
+  		getPrices: function () {
+  			return prices;
+  		},
+  		getDiscount: function() {
   			return discount;
   		},
   		setDiscount: function (newDiscount) {
   			discount = newDiscount;
-  			console.log(discount);
+  			prices.standard.discount = prices.standard.base*discount;
+  			prices.pro.discount = prices.pro.base*discount;
+  			prices.business.discount = prices.business.base*discount;
+  			// businessconsole.log(discount);
   		}
   	}
   });
